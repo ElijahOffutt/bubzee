4a568162bff4ace801ebf65bc6b99683fd114b2a
ElijahOffutt <Einzenheim@yahoo.com>
2023-11-18 16:07:13 -0500

    updates

--- components/bubzee.vue
+++ components/bubzee.vue
@@ -8,17 +8,19 @@
     <TransformControls :object="cube" />
 
     <!-- OBJECTS -->
+    <Levioso>
     <TresMesh ref="cube" :scale="1">
       <TresBoxGeometry :args="[1, 1, 1]" />
       <TresMeshNormalMaterial />
     </TresMesh>
+    </Levioso>
 
   </TresCanvas>
 </template>
 
 <script setup>
 import anime from 'animejs/lib/anime.es.js';
-import { OrbitControls, TransformControls, } from '@tresjs/cientos'
+import { OrbitControls, TransformControls, Levioso } from '@tresjs/cientos'
 
 const cube = shallowRef()
 
